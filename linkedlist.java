328cbcb555fb475f70e631d630b295278ea46dc1
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-07 19:15:44 +0530

    Set: to replace a value at an index

--- linkedlist.java
+++ linkedlist.java
@@ -5,6 +5,8 @@ public class linkedlist{
 		link.add(3);
 		link.add(532);
 		link.add(0, 2);
-		System.out.println(link);
+		System.out.println("Original Linkedlist: " + link);
+		link.set(0, -31);
+		System.out.println("First value swapped: " + link);
 	}
 }
