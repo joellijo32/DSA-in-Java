7d55a22c1427874b03c10276bfa8f30dde050a1c
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-07 17:55:37 +0530

    Created linkedlist of int type in java

---
+++ linkedlist.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class linkedlist{
+	public static void main(String args[]){
+		LinkedList<Integer> link = new LinkedList<Integer>();
+		link.add(3);
+		link.add(532);
+		System.out.println(link);
+	}
+}
