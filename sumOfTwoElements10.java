d8c97fb224d21b6b5b26c056092bb4758e261f12
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-24 12:25:13 +0530

    Alternate solution to Q1

--- sumOfTwoElements10.java
+++ sumOfTwoElements10.java
@@ -1,5 +1,19 @@
 import java.util.*;
 public class sumOfTwoElements10{
+
+    public int[] sum(int ar[], int target){
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < ar.length; i++){
+            int num = ar[i];
+            int match = target - num;
+            if(set.contains(match)){
+                return new int[]{num, match};
+            }
+            set.add(num);
+        }
+        return new int[0];
+    }
+
     public static void main(String agrs[]){
         Scanner sc = new Scanner(System.in);
         int n; System.out.print("Enter the size: "); n = sc.nextInt();
@@ -8,16 +22,13 @@ public class sumOfTwoElements10{
         for(int i = 0; i < n;i++) ar[i] = sc.nextInt();
         System.out.println("Entered Array: ");
         for(int i = 0; i < n; i++) System.out.print(ar[i] + " ");
-        System.out.print("\nElements Resulting Sum 10: ");
-
-        int a, b, sum = 0;
-        for(int i = 0; i < n-1;i++){
-            a = ar[i];
-            for(int j = i+1; j < n;j++ ){
-                b = ar[j];
-                if(a+b == 10) System.out.print("(" + a + ", " + b + ")  ");
-            }
+        sumOfTwoElements10 ob = new sumOfTwoElements10();
+        int result[] = ob.sum(ar, 10);
+        if(result.length == 0){
+            System.out.println("No matching pairs."); return;
         }
-        System.out.println();
+        for(int i = 0; i < result.length; i++) System.out.print("(" + result[0] + ", " + result[1] + ")  ");
+
+        
     }
 }
