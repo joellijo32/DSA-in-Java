41b84903637b5334d606a5184ef0940d9bceb246
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-25 10:12:40 +0530

    Created a linked list structure in java. Class for linked list

---
+++ singleLinkedList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class singleLinkedList {
+
+    public class Node{
+        int data; 
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+}
