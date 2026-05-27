68734c30d0ce31a739358aced350abeffbe0cc03
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-27 11:30:37 +0530

    Initialized a double linked list with class

---
+++ doubleLinkedList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class doubleLinkedList {
+    
+    public class Node{
+        Node prev, next;
+        int data;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+}
