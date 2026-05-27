4ee9076540645206ecab59e915058b87ac9538c9
Joel Lijo Mathew <joellijo4@gmail.com>
2026-05-27 11:43:52 +0530

    Adding nodes and displaying double linked list

--- doubleLinkedList.java
+++ doubleLinkedList.java
@@ -10,4 +10,50 @@ public class doubleLinkedList {
             this.data = data;
         }
     }
+
+    public Node head, tail;
+
+    void addNode(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            tail = head;
+            return;
+        }
+
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+
+    }
+
+    void display(){
+        Node temp = head;
+        if(temp == null){
+            System.out.println("Linked list is null."); 
+            return;
+        }
+        System.out.println("Linked List: ");
+        while(temp != null){
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("No. of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
+
+        doubleLinkedList ob = new doubleLinkedList();
+        for(int i = 0; i < n;i++){
+            ob.addNode(sc.nextInt());
+        }
+        ob.display();
+    }
+
+
 }
