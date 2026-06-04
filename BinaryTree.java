3d7c3f3b6fdf1061d3ffc59a1b6a5b49ea9544ee
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-04 18:01:01 +0530

    binary tree insertion and display complete

--- BinaryTree.java
+++ BinaryTree.java
@@ -15,6 +15,25 @@ public class BinaryTree {
 
     void insert(int data){
         Node currentNode = root;
+        if(root == null){
+            root = new Node(data);
+            return;
+        }
+        
+        Node newNode = new Node(data);
+        while(true){
+            if(currentNode.data > newNode.data){
+                if(currentNode.left == null)   {
+                    currentNode.left = newNode;
+                    break;
+                } else currentNode = currentNode.left;
+            } else {
+                if(currentNode.right == null){
+                    currentNode.right = newNode;
+                    break;
+                }else currentNode = currentNode.right;
+            }
+        }
     }
 
     boolean contains(int data){
@@ -27,4 +46,37 @@ public class BinaryTree {
         Node currentNode = root;
     }
 
+    void display(Node currentNode){
+        if(currentNode == null){
+            System.out.println("Null");
+            return;
+        }
+     
+            
+            System.out.println("Root: " + currentNode.data);
+       
+            System.out.println("Left : ");
+            display(currentNode.left);
+            System.out.println("Right: ");
+            display(currentNode.right);
+           
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Number of elements: ");
+        int n = sc.nextInt();
+
+        BinaryTree ob = new BinaryTree();
+        System.out.println("Enter the elements: ");
+        for(int i = 0; i < n;i++){
+            ob.insert(sc.nextInt());
+        } 
+
+        System.out.println("Binary Search Tree: ");
+        ob.display(ob.root);
+
+
+
+    }
 }
