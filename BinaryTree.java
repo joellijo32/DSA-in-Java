67930c6c858a22bdd1a1c85a7b004104baf3e6f6
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-04 17:00:47 +0530

    Binary tree class

---
+++ BinaryTree.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class BinaryTree {
+    
+    public class Node{
+        int data;
+        Node left, right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    Node root;
+}
