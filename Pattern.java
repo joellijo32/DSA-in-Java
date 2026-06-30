5c7ccb8ea793ed725dfa7f306186deeac771ca74
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 23:44:02 +0530

    Palindrom number traingle

--- Pattern.java
+++ Pattern.java
@@ -1,12 +1,24 @@
 public class Pattern {
     public static void main(String args[]){
-        int number = 1;
-        for(int i = 1; i <= 5; i++){
-            for(int j = 1; j <= 5; j++){
-                if(j >= (6-i) ) System.out.print(number + " ");
-                else System.out.print(" ");
-            } System.out.println();
-            number++;
+        int number = 1;int limit = 1;
+        for(int i = 0; i < 5; i++){
+            for(int j = 1; j <= 4-i; j++){
+                System.out.print("  ");
+            }
+            number = i+1;boolean isOneFound = false;
+            for(int j = 0; j < limit; j++){
+                System.out.print(number + " ");
+                if(!isOneFound){
+                    number--; 
+                    if(number == 0){
+                        number = 2;
+                        isOneFound = true;
+                    } 
+                } else number++;
+                
+            }
+            System.out.println();
+            limit = limit + 2;
         }
     }
 }
