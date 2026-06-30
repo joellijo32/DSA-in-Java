9bca9d665ec8e8b5dfbadd304cd2720ed854e10d
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 11:29:38 +0530

    inverted half pyramid

--- Pattern.java
+++ Pattern.java
@@ -1,8 +1,8 @@
 public class Pattern {
     public static void main(String args[]){
-        for(int i = 0 ; i < 4;i++){
-            for(int j = 0; j <= i; j++){
-                System.out.print("*");
+        for(int i = 0; i < 4;i++){
+            for(int j = 4; j > i; j--){
+                System.out.print("* ");
             } System.out.println();
         }
     }
