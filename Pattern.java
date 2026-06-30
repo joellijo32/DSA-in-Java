e90925113dacd9cb57ccdf18dedc58345b81bdf8
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 11:51:40 +0530

    Inverted Pyramid Rotated

--- Pattern.java
+++ Pattern.java
@@ -1,8 +1,10 @@
 public class Pattern {
     public static void main(String args[]){
-        for(int i = 0; i < 4;i++){
-            for(int j = 4; j > i; j--){
-                System.out.print("* ");
+        for(int i = 3; i >= 0; i--){
+            for(int j = 0; j < 4; j++){
+                if(j >= i) System.out.print("*");
+                else System.out.print(" ");
+                System.out.print(" ");
             } System.out.println();
         }
     }
