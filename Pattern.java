2e14f37b92b2e910215193e88ce86af3a4b362c1
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 12:06:56 +0530

    Half pyramid - Numbers

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,10 @@
 public class Pattern {
     public static void main(String args[]){
-        for(int i = 3; i >= 0; i--){
-            for(int j = 0; j < 4; j++){
-                if(j >= i) System.out.print("*");
-                else System.out.print(" ");
-                System.out.print(" ");
-            } System.out.println();
+        for(int i = 0; i < 5;i++){
+            for(int j  = 1; j <= i+1;j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
     }
 }
