7df3a6f8c944c2bc761a1add5d2d5094cd181c70
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 12:47:48 +0530

    Floyd's Triangle

--- Pattern.java
+++ Pattern.java
@@ -1,9 +1,12 @@
 public class Pattern {
     public static void main(String args[]){
-        for(int i = 5; i >= 1;i--){
-            for(int j = 1; j <= i; j++){
-                System.out.print(j + " ");
-            } System.out.println();
+        int number = 1;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
         }
     }
 }
