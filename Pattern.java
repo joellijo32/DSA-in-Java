a31c366b0b7724fe87aaf2b963bcc61cb952dcff
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 23:07:13 +0530

    Number Pyramid

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,12 @@
 public class Pattern {
     public static void main(String args[]){
-        for(int i = 0 ;i < 5;i++){
-            for(int j = 0 ; j < 9; j++){
-                if(j <= 8-i && j >= 4-i){
-                    System.out.print("* ");
-                } else System.out.print("  ");
+        int number = 1;
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= 5; j++){
+                if(j >= (6-i) ) System.out.print(number + " ");
+                else System.out.print(" ");
             } System.out.println();
+            number++;
         }
     }
 }
