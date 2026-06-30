e53a36374c0b6e3bb22494440380c896c85ee222
Joel Lijo Mathew <joellijo4@gmail.com>
2026-06-30 11:17:13 +0530

    solid rectangle

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
     public static void main(String args[]){
         for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 5;j++) System.out.print("* ");
+            for(int j = 0; j < 5;j++) System.out.print("*");
             System.out.println();
         }
     }
