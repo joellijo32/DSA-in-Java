4bb9fa12db9c7b71b2a6743e9762c80ed5e0812a
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 12:06:17 +0530

    Butterfly Patter - Complete

--- Pattern.java
+++ Pattern.java
@@ -1,18 +1,33 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 4;int spaceIdx = 2;
+        int n = 4;int spaceIdx = (n/2);
         for(int i = 0; i < n; i++){
             for(int j = 0; j < i+1; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
-            for(int j = 1; j <= (8-spaceIdx); j++){
-                System.out.print(" ");
+            for(int j = 1; j <= ((n*2)-spaceIdx); j++){
+                System.out.print("  ");
             }
             for(int j = 0; j < i+1; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
             spaceIdx+= 2;
+        }
+
+        spaceIdx = n*2;
+        for(int i = n; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+            for(int j = 1; j <= ((n*2)-spaceIdx); j++){
+                System.out.print("  ");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            spaceIdx -= 2;
         }
     }
 }
