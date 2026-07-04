ebad0d03a053a2d15f0f42f529fa25970030a4b7
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 14:13:24 +0530

    Number pyramid pattern dynamic wrt n

--- Pattern.java
+++ Pattern.java
@@ -1,17 +1,15 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 8;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < (n-i-1); j++){
-                System.out.print("  ");
+        int n = 6; int number = 1;
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < (n-i+1); j++){
+                System.out.print(" ");
             }
-            for(int j = 0; j < n; j++){
-                System.out.print("* ");
+            for(int j = 0; j < number; j++){
+                System.out.print(number + " ");
             }
-            for(int j = 0; j < i; j++){
-                System.out.print("  ");
-            }
-            System.out.println();
+            System.out.println(); 
+            number++;
         }
     }
 }
