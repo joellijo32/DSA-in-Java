a08bf688716b833e95b8191b242af6f0dce5758f
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 16:14:32 +0530

    Pascal's Triangle dynamic wrt n

--- Pattern.java
+++ Pattern.java
@@ -1,14 +1,20 @@
 public class Pattern {
+
+    int factorial(int n){
+        if(n == 0) return 1;
+        else return n*factorial(n-1);
+    }
+
     public static void main(String args[]){
+        Pattern ob = new Pattern();
         int n = 5;
-        for(int i = 0; i < n;i++){
-            for(int j = 0; j < (n-i-1); j++){
-                System.out.print("  ");
+        for(int i = 0; i < n;i++){  // nth row kth element.
+            for(int j = 0; j < n-i-1; j++){
+                System.out.print(" ");
             }
-            for(int j = 0; j < n; j++){
-                if(i == 0 || i == n-1) System.out.print("* ");
-                else if(j == 0 || j == n-1) System.out.print("* ");
-                else System.out.print("  ");
+            for(int j = 0; j < i+1; j++){
+                int number = (ob.factorial(i))/(ob.factorial(j) * (ob.factorial(i-j)));
+                System.out.print(number + " ");
             }
             System.out.println();
         }
