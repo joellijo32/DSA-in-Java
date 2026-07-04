3f1ad7c8a9e970913053a338ad369698c9ac2b1f
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 11:58:54 +0530

    Butterfly Pattern - Top part

--- Pattern.java
+++ Pattern.java
@@ -1,24 +1,18 @@
 public class Pattern {
     public static void main(String args[]){
-        int number = 1;int limit = 1;
-        for(int i = 0; i < 5; i++){
-            for(int j = 1; j <= 4-i; j++){
-                System.out.print("  ");
+        int n = 4;int spaceIdx = 2;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i+1; j++){
+                System.out.print("*");
             }
-            number = i+1;boolean isOneFound = false;
-            for(int j = 0; j < limit; j++){
-                System.out.print(number + " ");
-                if(!isOneFound){
-                    number--; 
-                    if(number == 0){
-                        number = 2;
-                        isOneFound = true;
-                    } 
-                } else number++;
-                
+            for(int j = 1; j <= (8-spaceIdx); j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i+1; j++){
+                System.out.print("*");
             }
             System.out.println();
-            limit = limit + 2;
+            spaceIdx+= 2;
         }
     }
 }
