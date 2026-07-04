64776ada9c5d37fdd16d4cd3da6575b49726b98e
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 14:56:49 +0530

    Diamond Pattern dynamic wrt n

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,21 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 9, number = 1; boolean isOneFound = false;
+        int n = 4;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < (n-i-1); j++){
+            for(int j = 0; j < n-i-1; j++){
                 System.out.print("  ");
             }
-            number = i+1; isOneFound = false;
             for(int j = 0; j < (2*i+1); j++){
-                System.out.print(number + " ");
-                if(number == 1){
-                    isOneFound = true;
-                }
-                if(isOneFound){
-                    number++;
-                } else number--;
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = n-1; i > 0; i--){
+            for(int j = 0; j < n-i; j++){
+                System.out.print("  ");
+            }
+            for(int j = 0; j < (2*i-1); j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
