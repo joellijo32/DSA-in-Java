315d3546049d57110719e4e6bb6aa329c47a1d4c
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 13:31:58 +0530

    Dynamic Solid Rhombus wrt n

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,11 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 5;
-        for(int i = 0; i < 5; i++){
+        int n = 8;
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < (n-i-1); j++){
                 System.out.print("  ");
             }
-            for(int j = 0; j < 5; j++){
+            for(int j = 0; j < n; j++){
                 System.out.print("* ");
             }
             for(int j = 0; j < i; j++){
