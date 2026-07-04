afa1b328bd6f84c46bd2cdd92721ac5cd9924099
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 14:24:50 +0530

    Dynamic Palindromic number pattern wrt n

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,21 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 6; int number = 1;
-        for(int i = 0; i < n;i++){
-            for(int j = 0; j < (n-i+1); j++){
-                System.out.print(" ");
+        int n = 9, number = 1; boolean isOneFound = false;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < (n-i-1); j++){
+                System.out.print("  ");
             }
-            for(int j = 0; j < number; j++){
+            number = i+1; isOneFound = false;
+            for(int j = 0; j < (2*i+1); j++){
                 System.out.print(number + " ");
+                if(number == 1){
+                    isOneFound = true;
+                }
+                if(isOneFound){
+                    number++;
+                } else number--;
             }
-            System.out.println(); 
-            number++;
+            System.out.println();
         }
     }
 }
