86ecd5b6436948ddb942c756133d2c9af537fa5e
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 15:49:20 +0530

    Hollow Rhombus, slanded

--- Pattern.java
+++ Pattern.java
@@ -1,31 +1,14 @@
 public class Pattern {
     public static void main(String args[]){
         int n = 5;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i+1; j++){
-                if(j == 0 || j == i) System.out.print("*");
-                else System.out.print(" ");
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < (n-i-1); j++){
+                System.out.print("  ");
             }
-            for(int j = 0; j < (2*(n-i-1)); j++){
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i+1; j++){
-                if(j == 0 || j == i) System.out.print("*");
-                else System.out.print(" ");
-            }
-            System.out.println();
-        }
-        for(int i = 5; i > 0; i--){
-            for(int j = 0; j < i; j++){
-                if(j == 0 | j == i-1) System.out.print("*");
-                else System.out.print(" ");
-            }
-            for(int j = 0; j < (2*(n-i)); j++){
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i; j++){
-                if(j == 0 | j == i-1) System.out.print("*");
-                else System.out.print(" ");
+            for(int j = 0; j < n; j++){
+                if(i == 0 || i == n-1) System.out.print("* ");
+                else if(j == 0 || j == n-1) System.out.print("* ");
+                else System.out.print("  ");
             }
             System.out.println();
         }
