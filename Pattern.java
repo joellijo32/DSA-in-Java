043cd812c58733549356b4ca44da222979dade31
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-04 13:26:54 +0530

    Solid Rhombus

--- Pattern.java
+++ Pattern.java
@@ -1,28 +1,15 @@
 public class Pattern {
     public static void main(String args[]){
-        int n = 8;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i+1; j++){
-                System.out.print("* ");
-            }
-            for(int j = 1; j <= (2*(n-(i+1))); j++){
+        int n = 5;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < (n-i-1); j++){
                 System.out.print("  ");
             }
-            for(int j = 0; j < i+1; j++){
+            for(int j = 0; j < 5; j++){
                 System.out.print("* ");
             }
-            System.out.println();
-        }
-
-        for(int i = n; i > 0; i--){
             for(int j = 0; j < i; j++){
-                System.out.print("* ");
-            }
-            for(int j = 1; j <= (2*(n-i)); j++){
                 System.out.print("  ");
-            }
-            for(int j = 0; j < i; j++){
-                System.out.print("* ");
             }
             System.out.println();
         }
