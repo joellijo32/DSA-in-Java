6184975f3cf44d6815ab4d4336437ce8e9cba77e
Joel Lijo Mathew <joellijo4@gmail.com>
2026-07-10 14:14:04 +0530

    Average of 3 numbers

---
+++ Functions/Average.java
@@ -0,0 +1,20 @@
+package Functions;
+import java.util.*;
+
+public class Average {
+
+    int average(int a, int b, int c){
+        return (a+b+c)/3;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 Num:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        Average ob = new Average();
+        System.out.println("Average: " + ob.average(a,b,c));
+    }
+}
